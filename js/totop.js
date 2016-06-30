fcd57e3f2d7e86fa96add6f9ee57b03aea267247
邓伟 <dengwei@myzaker.com>
2016-06-30 15:08:23 +0800

    published

--- js/totop.js
+++ js/totop.js
@@ -1,7 +1,7 @@
 (function($) {
     // When to show the scroll link
     // higher number = scroll link appears further down the page
-    var upperLimit = 500;
+    var upperLimit = 1600;
 
     // Our scroll link element
     var scrollElem = $('#totop');
